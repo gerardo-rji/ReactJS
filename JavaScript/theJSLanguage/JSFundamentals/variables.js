ceab8d511e0607f1256549c256f9cb12cdc17b95
Gerardo_RJI <gera.rji96@gmail.com>
2025-11-09 19:27:12 -0600

    work with variables and constants

---
+++ JavaScript/theJSLanguage/JSFundamentals/variables.js
@@ -0,0 +1,10 @@
+let name = 'John'
+let admin = name
+console.log(admin)
+
+let ourPlanet = 'Earth'
+let currentUser = 'John Doe'
+
+const BIRTHDAY = '1996-03-16'
+const someCode = (date) => {}
+const age = someCode(BIRTHDAY)
