0198b870cad71ae6545d6d5ab1bb1294e6bdd381
Gerardo_RJI <gera.rji96@gmail.com>
2025-12-02 01:09:11 -0600

    Functions

---
+++ JavaScript/theJSLanguage/JSFundamentals/functions.js
@@ -0,0 +1,32 @@
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+// Rewrite the function using '?' or '||'
+function checkAge(age) {
+  return age > 18 ? true : confirm('Did parents allow you?')
+}
+
+function checkAge2(age) {
+  return age > 18 || confirm('Did parents allow you?')
+}
+
+//  Function min(a, b)
+function min(a, b) {
+  return a < b ? a : b
+}
+
+// Function pow(x,n)
+function pow(x, n) {
+  return (n <= 0)
+    ? `Power ${n} is not supported, use a positive integer`
+    : x ** n
+}
+
+let x = await rl.question('x? ')
+let n = await rl.question('n? ')
+
+console.log(pow(x, n))
+
+rl.close();
