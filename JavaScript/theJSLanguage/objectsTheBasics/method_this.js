7d5e734260fbcbd08dfd6884f67c72e0104bb40e
Gerardo_RJI <gera.rji96@gmail.com>
2025-12-18 10:26:21 -0600

    Object methods, 'this'

---
+++ JavaScript/theJSLanguage/objectsTheBasics/method_this.js
@@ -0,0 +1,44 @@
+'use strict'
+
+// Create a calculator
+let claculator = {
+  read() {
+    this.a = +prompt('a? ', 0)
+    this.b = +prompt('b? ', 0)
+  },
+  sum() {
+    return this.a + this.b
+  },
+  mul() {
+    return this.a * this.b
+  }
+}
+
+calculator.read();
+alert(claculator.sum());
+alert(claculator.mul());
+
+// Chaining
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert( this.step );
+    return this;
+  }
+};
+
+ladder
+  .up()
+  .up()
+  .down()
+  .showStep() // 1
+  .down()
+  .showStep(); // 0 
